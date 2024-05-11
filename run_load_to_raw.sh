2f4992f748c3a5709d6dbf22e4d6b403f81ae2aa
sagar <sagar.uiet8@gmail.com>
2024-05-11 17:14:23 -0400

    Updating the shell script with appropriate commands

--- run_load_to_raw.sh
+++ run_load_to_raw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+figlet -f slant "Data Fusion"
 # Environment Variables
 # Get the current Google Cloud project ID
 CONFIG_PATH="utils/config.json"
@@ -11,19 +12,26 @@ if [ -z "$PROJECT_ID" ]; then
   exit 1
 fi
 
-echo "Using project: $PROJECT_ID"
+# echo "Using project: $PROJECT_ID"
 CLUSTER_NAME="data-fusion"
 REGION="us-east4"
 CODE_BUCKET=$(jq -r '.code_bucket' "$CONFIG_PATH")
-DATAPOC_BUCKET="df-dataproc"
-TRAFFIC_DATA_FILE="gs://${CODE_BUCKET}/scripts/taxi_data_landing_to_raw_pyspark.py"
-TAXI_DATA_FILE="gs://${CODE_BUCKET}/scripts/taxi_data_landing_to_raw_pyspark.py"
-CONFIG_FILE="gs://${CODE_BUCKET}/config/config.json"
-TRAFFIC_PYSPARK_JOB_NAME="load-traffic-raw-$(printf "%04d" $((RANDOM % 10000)))"
-TAXI_PYSPARK_JOB_NAME="load-traffic-raw-$(printf "%04d" $((RANDOM % 10000)))"
-BATCH_SIZE=2 # Maximum size in GB to be processed in one go, can be increased based on cluster config
+DATAPOC_BUCKET=$(jq -r '.dataproc_bucket' "$CONFIG_PATH")
+
+TRAFFIC_DATA_FILE="gs://${CODE_BUCKET}/scripts/load_traffic_data_pyspark.py"
+TRAFFIC_PYSPARK_JOB_NAME="load-traffic-data-raw-$(printf "%04d" $((RANDOM % 10000)))"
 TRAFFIC_DATA_PREFIX_PATH="data/pre-processed/traffic_data"
-TAXI_DATA_PREFIX_PATH="data/pre-processed/traffic_data"
+
+TAXI_DATA_FILE="gs://${CODE_BUCKET}/scripts/load_taxi_data_pyspark.py"
+TAXI_PYSPARK_JOB_NAME="load-taxi-data-raw-$(printf "%04d" $((RANDOM % 10000)))"
+TAXI_DATA_PREFIX_PATH="data/pre-processed/taxi_data"
+
+CRASHES_DATA_FILE="gs://${CODE_BUCKET}/scripts/load_crashes_data_pyspark.py"
+CRASHES_DATA_PREFIX_PATH="data/pre-processed/crashes_data"
+CRASHES_PYSPARK_JOB_NAME="load-crashes-data-raw-$(printf "%04d" $((RANDOM % 10000)))"
+
+BATCH_SIZE=2 # Maximum size in GB to be processed in one go, can be increased based on cluster config
+
 
 start_cluster(){
     gcloud dataproc clusters start $CLUSTER_NAME --region=$REGION
@@ -34,9 +42,6 @@ describe_cluster(){
     gcloud dataproc clusters describe $CLUSTER_NAME --region=$REGION
 }
 
-load_crashes_data(){
-python data_pipelines/load/direct_data_load_bigquery.py --process-name crashes_data
-}
 
 load_persons_data(){
 python data_pipelines/load/direct_data_load_bigquery.py --process-name persons_data
@@ -60,6 +65,11 @@ gcloud dataproc jobs submit pyspark --cluster=$CLUSTER_NAME --region=$REGION --p
 echo "Submitted PySpark job to Dataproc cluster."
 }
 
+load_crashes_data(){
+gcloud dataproc jobs submit pyspark --cluster=$CLUSTER_NAME --region=$REGION --project=$PROJECT_ID --id=$CRASHES_PYSPARK_JOB_NAME --files=$CONFIG_FILE --bucket=$DATAPROC_BUCKET $CRASHES_DATA_FILE -- --batch-size $BATCH_SIZE --prefix-path $CRASHES_DATA_PREFIX_PATH
+echo "Submitted PySpark job to Dataproc cluster."
+}
+
 stop_cluster(){
     gcloud dataproc clusters stop $CLUSTER_NAME --region=$REGION
     echo "Stopping the Dataproc cluster."
@@ -77,14 +87,20 @@ read -p "Enter your choice (1-6): " choice
 
 case $choice in
     1) load_weather_data ;;
-    2) load_crashes_data ;;
-    3) load_persons_data ;;
-    4) load_vehicles_data ;;
-    5)  submit_traffic_data_job
+    2)  start_cluster
+        load_crashes_data
         stop_cluster
         exit 0
         ;;
-    6) submit_taxi_data_job
+    3) load_persons_data ;;
+    4) load_vehicles_data ;;
+    5)  start_cluster
+        load_traffic_data
+        stop_cluster
+        exit 0
+        ;;
+    6)  start_cluster
+        load_taxi_data
         stop_cluster
         exit 0
         ;;
