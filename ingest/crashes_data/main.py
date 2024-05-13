d830b31a508f2ed130aa8ee216b1d6d49fd07543
dhirajlahoti <dhirajlahoti25@gmail.com>
2024-05-13 12:52:39 -0400

    added docstring/comments and updated ingest script for persons, crashes

--- ingest/crashes_data/main.py
+++ ingest/crashes_data/main.py
@@ -29,65 +29,137 @@ DEFAULT_END_DATE = '2012-07-31T23:59:59'
 
 
 def fetch_last_offset(table_id):
+    """
+    Fetches the last timestamp loaded from a specified table.
+
+    Args:
+        table_id (str): ID of the table from which to fetch the last timestamp.
+
+    Returns:
+        str: last timestamp loaded from the table or the default start date if no timestamp is found.
+
+    """
     query = f"""
     SELECT last_timestamp_loaded FROM `{table_id}`
     where process_name='{PROCESS_NAME}' and process_status='Success'
     ORDER BY insert_ts desc
     LIMIT 1
     """
+    # Execute the query and retrieve results
     results = bq_client.query(query).result()
     try:
         offset = list(results)[0]["last_timestamp_loaded"]
+        # Return the extracted offset if it exists, otherwise return the default start date
         return offset if offset else DEFAULT_START_DATE
     except Exception:
-        return DEFAULT_START_DATE
     
-
+        return DEFAULT_START_DATE
+      
 def get_dates(input_date):
+    """
+    Converts the input date to a start date and an end date.
+
+    Args:
+        input_date (datetime): The input date to be converted.
+
+    Returns:
+        tuple: A tuple containing the start date and end date.
+    """
     if isinstance(input_date, str):
+        # If input_date is a string, convert it to a datetime object
         start_date = datetime.datetime.strptime(input_date, "%Y-%m-%dT%H:%M:%S")
     elif isinstance(input_date, datetime.datetime):
+        # If input_date is already a datetime object, use it as is
         start_date = input_date
+    
+    # Calculate the end date by adding the specified number of days
     end_date = start_date + datetime.timedelta(days=DAY_DELTA)
+    
+    # Convert start_date and end_date to ISO 8601 format
     start_date = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     end_date = end_date.strftime("%Y-%m-%dT%H:%M:%S")
+    
     return start_date, end_date
 
 
 def fetch_data(start_date, end_date):
+    """
+    Fetches data from an API based on the specified date range.
+
+    Args:
+        start_date (str): The start date of the data range in ISO 8601 format ("%Y-%m-%dT%H:%M:%S").
+        end_date (str): The end date of the data range in ISO 8601 format ("%Y-%m-%dT%H:%M:%S").
+
+    Returns:
+        DataFrame or None: A DataFrame containing the fetched data if successful, otherwise None.
+    """
     try:
+        # Construct parameters for API request
         params = {
             '$limit': f"{LIMIT}",
             '$where': f"crash_date >= '{start_date}' AND crash_date <= '{end_date}'"
         }
+        # Encode parameters for URL and construct API URL
         encoded_params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote)
         api_url = f"{BASE_URL}?{encoded_params}"
+        
+        # Read data from API into a DataFrame
         df = pd.read_csv(api_url, low_memory=False)
         return df
     except requests.RequestException as e:
         print(f"Request failed: {e}")
+    
+    # Return None if there was an error fetching the data
     return None
 
 
 def upload_to_gcs(data):
+    """
+    Uploads data to Google Cloud Storage in CSV format.
+
+    Args:
+        data (Data Frame): The data to be uploaded.
+
+    Returns:
+        str: A string indicating the status of the upload operation. Possible values are "Success" or "Failure".
+
+    """
+    # Get the current day and timestamp
     current_day = datetime.date.today()
     current_timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    
+    # Define the file path and name
     file_path = f"{BASE_FILE_PATH}/{current_day}"
     file_name = f"{BASE_PROC_NAME}_{current_timestamp}.csv"
+    
     try:
+        # Upload the data to GCS
         data.to_csv(f"gs://{LANDING_BUCKET}/{file_path}/{file_name}", index=False)
         return "Success"
     except Exception as e:
+        # Print the error message if an exception occurs during the upload
         print(e)
         return "Failure"
 
 
 def store_func_state(table_id, state_json):
+    """
+    Stores function state JSON data into a specified BigQuery table.
+
+    Args:
+        table_id (str): The ID of the BigQuery table where the data will be stored.
+        state_json (dict): The function state JSON data to be stored.
+
+    """
+    # Prepare data for insertion into BigQuery table
     rows_to_insert = [state_json]
     errors = bq_client.insert_rows_json(table_id, rows_to_insert)
+    
+    # Check if insertion was successful or not
     if not errors:
         print("New rows have been added.")
     else:
+        # Print any insertion errors
         print(f"Insert errors: {errors}")
 
 
--- ingest/persons_data/main.py
+++ ingest/persons_data/main.py
@@ -25,66 +25,158 @@ BASE_PROC_NAME = "persons_data"
 
 
 def fetch_last_offset(table_id):
+    """
+    Fetches the last timestamp loaded from a specified table in BigQuery.
+
+    Args:
+        table_id (str): The ID of the table in BigQuery from which to fetch the last timestamp.
+
+    Returns:
+        str: The last timestamp loaded, or the default start date if no timestamp is found.
+        
+    """
+    # Construct SQL query to select the last loaded timestamp from the specified table
     query = f"""
-    SELECT last_timestamp_loaded FROM `{table_id}`
-    where process_name='{PROCESS_NAME}' and process_status='Success'
-    ORDER BY insert_ts desc
+    SELECT last_timestamp_loaded  FROM `{table_id}`
+    WHERE process_name='{PROCESS_NAME}' AND process_status='Success'
+    ORDER BY insert_ts DESC
     LIMIT 1
     """
+
+    # Execute the query and retrieve results
     results = bq_client.query(query).result()
+
     try:
+        # Extract the last timestamp loaded from the query results
         offset = list(results)[0]["last_timestamp_loaded"]
+        # If offset is None or empty, return default start date
         return offset if offset else DEFAULT_START_DATE
     except Exception:
         return DEFAULT_START_DATE
+
     
 
 def get_dates(input_date):
+    """
+    Generates start and end dates based on the input date.
+
+    Args:
+        input_date (datetime): The input date. If a string, it should be in this format: "%Y-%m-%dT%H:%M:%S".
+
+    Returns:
+        tuple: A tuple containing the start date and end date in this format: "%Y-%m-%dT%H:%M:%S".
+
+    """
+    # If input_date is a string, convert it to a datetime object
     if isinstance(input_date, str):
         start_date = datetime.datetime.strptime(input_date, "%Y-%m-%dT%H:%M:%S")
+    # If input_date is already a datetime object, use it directly
     elif isinstance(input_date, datetime.datetime):
         start_date = input_date
+
+    # Calculate the end date by adding DAY_DELTA days to the start date
     end_date = start_date + datetime.timedelta(days=DAY_DELTA)
+
+    # Format start_date and end_date as strings in the required format
     start_date = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     end_date = end_date.strftime("%Y-%m-%dT%H:%M:%S")
+
     return start_date, end_date
 
 
 def fetch_data(start_date, end_date):
+    """
+    Fetches data from an API endpoint within a specified date range.
+
+    Args:
+        start_date (str): The start date of the date range in the format 'YYYY-MM-DD'.
+        end_date (str): The end date of the date range in the format 'YYYY-MM-DD'.
+
+    Returns:
+        pandas.DataFrame or None: A DataFrame containing the fetched data, or None if the request fails.
+
+    """
     try:
+        # Define parameters for the API request
         params = {
             '$limit': f"{LIMIT}",
             '$where': f"crash_date >= '{start_date}' AND crash_date <= '{end_date}'"
         }
+        
+        # Encode the parameters for use in the URL
         encoded_params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote)
+        
+        # Construct the API URL with the encoded parameters
         api_url = f"{BASE_URL}?{encoded_params}"
+        
+        # Read data from the API URL into a DataFrame
         df = pd.read_csv(api_url, low_memory=False)
+        
+        # Return the DataFrame
         return df
     except requests.RequestException as e:
+        # If the request fails, print an error message and return None
         print(f"Request failed: {e}")
     return None
 
 
 def upload_to_gcs(data):
+    """
+    Uploads a DataFrame to Google Cloud Storage (GCS).
+
+    Args:
+        data (pandas.DataFrame): The DataFrame to be uploaded to GCS.
+
+    Returns:
+        str: "Success" if the upload is successful, "Failure" otherwise.
+
+    Raises:
+        None
+    """
+    # Get the current date and timestamp
     current_day = datetime.date.today()
     current_timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    
+    # Define the file path and name for the uploaded file
     file_path = f"{BASE_FILE_PATH}/{current_day}"
     file_name = f"{BASE_PROC_NAME}_{current_timestamp}.csv"
+    
     try:
+        # Upload the DataFrame to GCS using a URL like gs://<bucket_name>/<file_path>/<file_name>
         data.to_csv(f"gs://{LANDING_BUCKET}/{file_path}/{file_name}", index=False)
+        # Return "Success" if the upload is successful
         return "Success"
     except Exception as e:
+        # If an exception occurs during the upload, print the error message and return "Failure"
         print(e)
         return "Failure"
 
 
 def store_func_state(table_id, state_json):
+    """
+    Stores the state of a function in a specified BigQuery table.
+
+    Args:
+        table_id (str): The ID of the BigQuery table where the function state will be stored.
+        state_json (dict): The JSON object representing the state of the function.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    # Prepare the data to be inserted into the BigQuery table
     rows_to_insert = [state_json]
+    
     errors = bq_client.insert_rows_json(table_id, rows_to_insert)
+    
+    # Check if there are any errors during insertion
     if not errors:
         print("New rows have been added.")
     else:
         print(f"Insert errors: {errors}")
+
 
 
 @functions_framework.http
